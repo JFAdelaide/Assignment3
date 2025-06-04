1ecc16f9b5055eef98890787a15ffd269cbd1385
Jesulous <JesseFabian99@gmail.com>
2025-06-04 13:38:24 +0930

    Added skeletal functions to code

---
+++ DistanceVector.py
@@ -0,0 +1,24 @@
+import sys
+from collections import defaultdict
+import copy
+
+def read_topology():
+    return
+
+def initialise_tables(nodes):
+    return
+
+def print_distance_tables(step, distance_tables, nodes):
+    return
+
+def print_routing_tables(routing_tables, nodes):
+    return
+
+def distance_vector(graph, nodes):
+    return
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
